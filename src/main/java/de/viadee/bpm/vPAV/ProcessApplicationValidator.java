9751266404725f3e0d946d0dbb1913f1060bbed1
Marcel Flasskamp <Marcel.Flasskamp@viadee.de>
2017-08-18 13:14:56 +0200

    VPAV-108
    new Method to find all errors in model

--- src/main/java/de/viadee/bpm/vPAV/ProcessApplicationValidator.java
+++ src/main/java/de/viadee/bpm/vPAV/ProcessApplicationValidator.java
@@ -1,18 +1,21 @@
 package de.viadee.bpm.vPAV;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 import org.springframework.context.ApplicationContext;
 
 import de.viadee.bpm.vPAV.beans.BeanMappingGenerator;
 import de.viadee.bpm.vPAV.processing.model.data.CheckerIssue;
+import de.viadee.bpm.vPAV.processing.model.data.CriticalityEnum;
 
 public class ProcessApplicationValidator extends AbstractRunner {
 
-    /*
-     * run vPAV with given ApplicationContext (Spring)
-     *
-     * @param ApplicationContext
+    /**
+     * find issues with given ApplicationContext (Spring)
+     * 
+     * @param ctx
+     * @return all issues
      */
     public static Collection<CheckerIssue> findModelInconsistencies(ApplicationContext ctx) {
 
@@ -23,8 +26,25 @@ public class ProcessApplicationValidator extends AbstractRunner {
         return AbstractRunner.getfilteredIssues();
     }
 
-    /*
-     * run vPAV (no Spring)
+    /**
+     * find issues with given ApplicationContext (Spring)
+     * 
+     * @param ctx
+     * @return issues with status error
+     */
+    public static Collection<CheckerIssue> findModelErrors(ApplicationContext ctx) {
+
+        RuntimeConfig.getInstance().setBeanMapping(BeanMappingGenerator.generateBeanMappingFile(ctx));
+        RuntimeConfig.getInstance().setClassLoader(ProcessApplicationValidator.class.getClassLoader());
+        run_vPAV();
+
+        return filterErrors(AbstractRunner.getfilteredIssues(), CriticalityEnum.ERROR);
+    }
+
+    /**
+     * find model errors without spring context
+     * 
+     * @return all issues
      */
     public static Collection<CheckerIssue> findModelInconsistencies() {
 
@@ -34,4 +54,35 @@ public class ProcessApplicationValidator extends AbstractRunner {
         return AbstractRunner.getfilteredIssues();
     }
 
+    /**
+     * find model errors without spring context
+     * 
+     * @return issues with status error
+     */
+    public static Collection<CheckerIssue> findModelErrors() {
+
+        RuntimeConfig.getInstance().setClassLoader(ProcessApplicationValidator.class.getClassLoader());
+        run_vPAV();
+
+        return filterErrors(AbstractRunner.getfilteredIssues(), CriticalityEnum.ERROR);
+    }
+
+    /**
+     * filter an issue collection by status
+     * 
+     * @param filteredIssues
+     * @param status
+     * @return issues with status
+     */
+    private static Collection<CheckerIssue> filterErrors(Collection<CheckerIssue> filteredIssues,
+            CriticalityEnum status) {
+        Collection<CheckerIssue> filteredErrors = new ArrayList<CheckerIssue>();
+
+        for (CheckerIssue issue : filteredIssues) {
+            if (issue.getClassification().equals(status)) {
+                filteredErrors.add(issue);
+            }
+        }
+        return filteredErrors;
+    }
 }
