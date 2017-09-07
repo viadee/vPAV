d2b2e0598c1c563168b1457a92a246b3330e1947
Marcel Flasskamp <Marcel.Flasskamp@viadee.de>
2017-09-07 12:01:18 +0200

    VPAV-110
    add info to message

--- src/main/java/de/viadee/bpm/vPAV/processing/checker/ProcessVariablesModelChecker.java
+++ src/main/java/de/viadee/bpm/vPAV/processing/checker/ProcessVariablesModelChecker.java
@@ -1,31 +1,22 @@
 /**
- * Copyright � 2017, viadee Unternehmensberatung GmbH
- * All rights reserved.
+ * Copyright � 2017, viadee Unternehmensberatung GmbH All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *    This product includes software developed by the viadee Unternehmensberatung GmbH.
- * 4. Neither the name of the viadee Unternehmensberatung GmbH nor the
- *    names of its contributors may be used to endorse or promote products
- *    derived from this software without specific prior written permission.
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met: 1. Redistributions of source code must retain the above copyright notice, this list of
+ * conditions and the following disclaimer. 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation and/or other materials provided with the
+ * distribution. 3. All advertising materials mentioning features or use of this software must display the following
+ * acknowledgement: This product includes software developed by the viadee Unternehmensberatung GmbH. 4. Neither the
+ * name of the viadee Unternehmensberatung GmbH nor the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY <viadee Unternehmensberatung GmbH> ''AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY <viadee Unternehmensberatung GmbH> ''AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package de.viadee.bpm.vPAV.processing.checker;
 
@@ -72,7 +63,7 @@ public class ProcessVariablesModelChecker implements ModelChecker {
                             anomaly.getAnomaly(), paths,
                             "process variable (" + var.getName() + ") will be overwritten in activity '"
                                     + var.getElement().getBaseElement().getAttributeValue("name")
-                                    + " without use. (compare: "
+                                    + " without use. (compare model: "
                                     + var.getChapter() + ", " + var.getFieldType().getDescription() + ")"));
                 } else if (anomaly.getAnomaly() == Anomaly.DU) {
                     issues.add(new CheckerIssue(rule.getName(), determineCriticality(anomaly.getAnomaly()),
@@ -82,7 +73,7 @@ public class ProcessVariablesModelChecker implements ModelChecker {
                             anomaly.getAnomaly(), paths,
                             "process variable (" + var.getName() + ") will be deleted in activity '"
                                     + var.getElement().getBaseElement().getAttributeValue("name")
-                                    + "' without use. (compare: "
+                                    + "' without use. (compare model: "
                                     + var.getChapter() + ", " + var.getFieldType().getDescription() + ")"));
                 } else if (anomaly.getAnomaly() == Anomaly.UR) {
                     issues.add(new CheckerIssue(rule.getName(), determineCriticality(anomaly.getAnomaly()),
@@ -92,7 +83,7 @@ public class ProcessVariablesModelChecker implements ModelChecker {
                             anomaly.getAnomaly(), paths,
                             "there is a read access to variable (" + var.getName() + ") in activity '"
                                     + var.getElement().getBaseElement().getAttributeValue("name")
-                                    + "', but no value has been set before. (compare: "
+                                    + "', but no value has been set before. (compare model: "
                                     + var.getChapter() + ", " + var.getFieldType().getDescription() + ")"));
                 }
             }
