b56f3b695402d64a31b79d458afc2d7c33c7f285
Sascha Di Bernardo <sascha.dibernardo@viadee.de>
2017-09-29 12:50:23 +0200

    vPAV-128 Groomed the FileScanner and deleted obsolete code

--- src/main/java/de/viadee/bpm/vPAV/FileScanner.java
+++ src/main/java/de/viadee/bpm/vPAV/FileScanner.java
@@ -89,7 +89,7 @@ public class FileScanner {
 
     public static Logger logger = Logger.getLogger(FileScanner.class.getName());
 
-    public FileScanner(final Map<String, Rule> rules, final String classPathScanLocation) {
+    public FileScanner(final Map<String, Rule> rules) {
 
         final DirectoryScanner scanner = new DirectoryScanner();
         scanner.setBasedir(ConstantsConfig.BASEPATH);
@@ -104,72 +104,6 @@ public class FileScanner {
         scanner.setIncludes(new String[] { ConstantsConfig.JAVA_FILE_PATTERN });
         scanner.scan();
         javaResourcesFileInputStream = new HashSet<String>(Arrays.asList(scanner.getIncludedFiles()));
-
-        // get mapping from process id to file path
-        processIdToPathMap = createProcessIdToPathMap(processdefinitions);
-
-        // get file paths of java files
-        if (classPathScanLocation != null && !classPathScanLocation.isEmpty()) {
-            URL[] urls;
-            LinkedList<File> files = new LinkedList<File>();
-
-            URLClassLoader ucl;
-            if (RuntimeConfig.getInstance().getClassLoader() instanceof URLClassLoader) {
-                ucl = ((URLClassLoader) RuntimeConfig.getInstance().getClassLoader());
-            } else {
-                ucl = ((URLClassLoader) RuntimeConfig.getInstance().getClassLoader().getParent());
-            }
-            urls = ucl.getURLs();
-
-            // retrieve all jars during runtime and pass them to get class files
-            for (URL url : urls) {
-                if (url.getFile().contains(targetClassFolder)) {
-                    File f = new File(url.getFile() + classPathScanLocation);
-                    if (f.exists()) {
-                        files = (LinkedList<File>) FileUtils.listFiles(f,
-                                TrueFileFilter.INSTANCE,
-                                TrueFileFilter.INSTANCE);
-                        addResources(files);
-                    }
-                }
-            }
-        }
-
-        // get mapping from decision reference to file path
-        scanner.setIncludes(new String[] { ConstantsConfig.DMN_FILE_PATTERN });
-        scanner.scan();
-        decisionRefToPathMap = createDmnKeyToPathMap(
-                new HashSet<String>(Arrays.asList(scanner.getIncludedFiles())));
-
-        // determine version name schema for resources
-        String versioningSchema = null;
-
-        try {
-            versioningSchema = loadVersioningSchemaClass(rules);
-        } catch (ConfigItemNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        if (versioningSchema != null) {
-            // also add groovy files to included files
-            scanner.setIncludes(new String[] { ConstantsConfig.SCRIPT_FILE_PATTERN });
-            scanner.scan();
-            includedFiles.addAll(Arrays.asList(scanner.getIncludedFiles()));
-
-            // filter files by versioningSchema
-            resourcesNewestVersions = createResourcesToNewestVersions(includedFiles, versioningSchema);
-        }
-    }
-
-    public FileScanner(final Map<String, Rule> rules) {
-
-        final DirectoryScanner scanner = new DirectoryScanner();
-        scanner.setBasedir(ConstantsConfig.BASEPATH);
-
-        // get file paths of process definitions
-        scanner.setIncludes(new String[] { ConstantsConfig.BPMN_FILE_PATTERN });
-        scanner.scan();
-        processdefinitions = new HashSet<String>(Arrays.asList(scanner.getIncludedFiles()));
 
         // get mapping from process id to file path
         processIdToPathMap = createProcessIdToPathMap(processdefinitions);
@@ -198,7 +132,6 @@ public class FileScanner {
                     addResources(files);
                 }
             }
-
         }
 
         // get mapping from decision reference to file path
