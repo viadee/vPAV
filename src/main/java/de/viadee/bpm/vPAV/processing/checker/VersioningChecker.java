0ada2141f510137a8be411840c1176bcc86d2ce4
Sascha Di Bernardo <sascha.dibernardo@viadee.de>
2017-10-09 13:26:11 +0200

    vPAV-133 Fixed HTML output for VersioningChecker

--- src/main/java/de/viadee/bpm/vPAV/processing/checker/VersioningChecker.java
+++ src/main/java/de/viadee/bpm/vPAV/processing/checker/VersioningChecker.java
@@ -1,31 +1,22 @@
 /**
- * Copyright � 2017, viadee Unternehmensberatung GmbH
- * All rights reserved.
+ * Copyright � 2017, viadee Unternehmensberatung GmbH All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *    This product includes software developed by the viadee Unternehmensberatung GmbH.
- * 4. Neither the name of the viadee Unternehmensberatung GmbH nor the
- *    names of its contributors may be used to endorse or promote products
- *    derived from this software without specific prior written permission.
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met: 1. Redistributions of source code must retain the above copyright notice, this list of
+ * conditions and the following disclaimer. 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation and/or other materials provided with the
+ * distribution. 3. All advertising materials mentioning features or use of this software must display the following
+ * acknowledgement: This product includes software developed by the viadee Unternehmensberatung GmbH. 4. Neither the
+ * name of the viadee Unternehmensberatung GmbH nor the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY <viadee Unternehmensberatung GmbH> ''AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY <viadee Unternehmensberatung GmbH> ''AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package de.viadee.bpm.vPAV.processing.checker;
 
@@ -77,7 +68,6 @@ public class VersioningChecker extends AbstractElementChecker {
     public Collection<CheckerIssue> check(final BpmnElement element) {
 
         final Collection<CheckerIssue> issues = new ArrayList<CheckerIssue>();
-
         final BaseElement baseElement = element.getBaseElement();
 
         // Service Task, Business Task, Send Task
@@ -99,8 +89,8 @@ public class VersioningChecker extends AbstractElementChecker {
 
         // Message Event Definition
         issues.addAll(checkMessageEventDefinition(element));
-
         return issues;
+
     }
 
     /**
@@ -237,6 +227,7 @@ public class VersioningChecker extends AbstractElementChecker {
     private Collection<CheckerIssue> checkMessageEventDefinition(final BpmnElement element) {
         final Collection<CheckerIssue> issues = new ArrayList<CheckerIssue>();
         final BaseElement baseElement = element.getBaseElement();
+
         if (baseElement instanceof MessageEventDefinition) {
             // Class, Expression, Delegate Expression
             final MessageEventDefinition eventDef = (MessageEventDefinition) baseElement;
@@ -369,6 +360,16 @@ public class VersioningChecker extends AbstractElementChecker {
             final Collection<CheckerIssue> issues) {
         if (javaReference != null) {
             if (!resourcesNewestVersions.contains(javaReference)) {
+                if (element.getBaseElement().getId() == null) {
+                    issues.add(new CheckerIssue(rule.getName(), CriticalityEnum.WARNING,
+                            element.getProcessdefinition(), javaReference,
+                            element.getBaseElement().getParentElement().getAttributeValue("id"),
+                            element.getBaseElement().getParentElement().getAttributeValue("name"), null, null, null,
+                            "class reference is deprecated or file with version does not exist for class '"
+                                    + javaReference + "'"));
+                }
+            } else {
+
                 issues.add(new CheckerIssue(rule.getName(), CriticalityEnum.WARNING,
                         element.getProcessdefinition(), javaReference, element.getBaseElement().getId(),
                         element.getBaseElement().getAttributeValue("name"), null, null, null,
