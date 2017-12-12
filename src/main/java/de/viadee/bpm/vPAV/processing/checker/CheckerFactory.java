2a47982b29d30cd07e03372ee170aedd90e7929c
Marcel Flasskamp <Marcel.Flasskamp@viadee.de>
2017-12-12 15:04:21 +0100

    VPAV-160
    deactivate checker if not found

--- src/main/java/de/viadee/bpm/vPAV/processing/checker/CheckerFactory.java
+++ src/main/java/de/viadee/bpm/vPAV/processing/checker/CheckerFactory.java
@@ -134,9 +134,11 @@ public final class CheckerFactory {
             fullyQualifiedName = rule.getValue().getSettings().get(externLocation).getValue()
                     + "." + rule.getValue().getName().trim();
         }
-        if (fullyQualifiedName.isEmpty() && rule.getValue().isActive())
+        if (fullyQualifiedName.isEmpty() && rule.getValue().isActive()) {
             logger.warning("Checker '" + rule.getValue().getName()
                     + "' not found. Please add setting for external_location in ruleSet.xml.");
+            rule.getValue().deactivate();
+        }
         return fullyQualifiedName;
     }
 }
