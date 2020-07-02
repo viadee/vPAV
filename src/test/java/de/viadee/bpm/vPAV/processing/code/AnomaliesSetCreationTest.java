b5b5ada3ccb2bb18e157b0b797c266ccbe8a3c13
Tamara Gunkel <tamara.nrw@web.de>
2020-07-03 00:11:19 +0200

    fix running test locally

--- src/test/java/de/viadee/bpm/vPAV/processing/code/AnomaliesSetCreationTest.java
+++ src/test/java/de/viadee/bpm/vPAV/processing/code/AnomaliesSetCreationTest.java
@@ -37,6 +37,7 @@ import de.viadee.bpm.vPAV.RuntimeConfig;
 import de.viadee.bpm.vPAV.config.model.RuleSet;
 import de.viadee.bpm.vPAV.constants.ConfigConstants;
 import de.viadee.bpm.vPAV.processing.ElementGraphBuilder;
+import de.viadee.bpm.vPAV.processing.JavaReaderStatic;
 import de.viadee.bpm.vPAV.processing.ProcessVariablesScanner;
 import de.viadee.bpm.vPAV.processing.code.flow.FlowAnalysis;
 import de.viadee.bpm.vPAV.processing.model.data.Anomaly;
@@ -47,6 +48,7 @@ import org.camunda.bpm.model.bpmn.BpmnModelInstance;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import soot.Scene;
 
 import java.io.File;
 import java.io.IOException;
@@ -62,14 +64,17 @@ public class AnomaliesSetCreationTest {
 
     @BeforeClass
     public static void setup() throws IOException {
+        RuntimeConfig.getInstance().setTest(true);
+        FileScanner.setupSootClassPaths(new LinkedList<>());
+        new JavaReaderStatic().setupSoot();
+        Scene.v().loadNecessaryClasses();
+
         final File file = new File(".");
         final String currentPath = file.toURI().toURL().toString();
         final URL classUrl = new URL(currentPath + "src/test/java");
         final URL[] classUrls = { classUrl };
         ClassLoader cl = new URLClassLoader(classUrls);
         RuntimeConfig.getInstance().setClassLoader(cl);
-        RuntimeConfig.getInstance().getResource("en_US");
-        RuntimeConfig.getInstance().setTest(true);
     }
 
     @Before
