0e4565cf61b46619530132fcc683951b82193910
Alexey Falkunskyy <a.falkunskyy@freenet.de>
2020-08-11 12:01:24 +0200

    Added test for validation checks

--- src/test/java/de/viadee/bpm/vPAV/config/reader/PropertiesReaderTest.java
+++ src/test/java/de/viadee/bpm/vPAV/config/reader/PropertiesReaderTest.java
@@ -1,5 +1,6 @@
 package de.viadee.bpm.vPAV.config.reader;
 
+import de.viadee.bpm.vPAV.exceptions.InvalidPropertiesConfiguration;
 import de.viadee.bpm.vPAV.exceptions.InvalidPropertiesParameterException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,18 +10,25 @@ import org.mockito.Answers;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.*;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.configuration.ConfigurationType.PowerMock;
 
 @RunWith(MockitoJUnitRunner.class)
 class PropertiesReaderTest {
 
     @Mock(answer = Answers.CALLS_REAL_METHODS)
     PropertiesReader testSubject;
-
     Properties properties;
 
     @BeforeEach
@@ -28,10 +36,6 @@ class PropertiesReaderTest {
         properties = new Properties();
         MockitoAnnotations.initMocks(this);
         when(testSubject.readPropertiesFromFile()).thenReturn(properties);
-    }
-
-    @AfterEach
-    void tearDown() {
     }
 
     @Test
@@ -55,4 +59,53 @@ class PropertiesReaderTest {
             testSubject.initProperties();
         });
     }
+
+    @Test
+    void testMultiReportsEnabledWhenOutputHtmlIsDisabled() {
+        properties.put("multiProjectReport", "true");
+        properties.put("outputhtml", "false");
+        assertThrows(InvalidPropertiesConfiguration.class, () -> {
+            testSubject.initProperties();
+        });
+    }
+
+    @Test
+    void testOnlyOneExternalReportPath() {
+        properties.put("multiProjectReport", "true");
+        properties.put("generatedReports", "./a/b");
+        assertThrows(InvalidPropertiesParameterException.class, () -> {
+            testSubject.initProperties();
+        });
+    }
+
+    //TODO Static Mocking, maybe with Powermock?
+    //    @Test
+    //    void testValidationReportNotFound() {
+    //        properties.put("multiProjectReport", "true");
+    //        properties.put("generatedReports", "./a/b,/c/d");
+    //        mockStatic(Files.class);
+    //        doReturn(false)
+    //        when(Files.exists(Paths.get("foo"), new LinkOption[] { LinkOption.NOFOLLOW_LINKS })).thenReturn(false);
+    //        assertThrows(InvalidPropertiesParameterException.class, () -> {
+    //            testSubject.initProperties();
+    //        });
+    //    }
+
+    @Test
+    void testMultiReportsEnabledWithoutGeneratedReportsPaths() {
+        properties.put("multiProjectReport", "true");
+        assertThrows(InvalidPropertiesConfiguration.class, () -> {
+            testSubject.initProperties();
+        });
+    }
+
+    @Test
+    void testMultiReportsDisabeldWithGeneratedReportsPaths() {
+        properties.put("multiProjectReport", "false");
+        properties.put("generatedReports", "./a/b");
+        assertThrows(InvalidPropertiesConfiguration.class, () -> {
+            testSubject.initProperties();
+        });
+    }
+
 }
