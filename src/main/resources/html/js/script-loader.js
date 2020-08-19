aa0f3ba7c7550c762aba2116e4498b6b357d0a09
Alexey Falkunskyy <a.falkunskyy@freenet.de>
2020-08-19 10:33:12 +0200

    Made script loading reusable form main app

--- src/main/resources/html/js/script-loader.js
+++ src/main/resources/html/js/script-loader.js
@@ -1,16 +1,34 @@
-[    //generated data
-    "data/checkers.js", "data/bpmn_model.js", "data/bpmn_validation.js", "data/bpmn_validation_success.js",
-    "data/infoPOM.js", "data/issue_severity.js", "data/ignoredIssues.js",
-    "data/processVariables.js",
-    //bootstrap with dependencies
-    "js/jquery-3.5.1.min.js", "js/bootstrap.bundle.min.js",
-    //bpmn-js viewer
-    "js/bpmn-navigated-viewer.js",
-    //application
-    "js/download.js", "data/properties.js", "js/bpmn.io.viewer.app.js"]
-    .forEach(scriptSrc => {
+function generateJsDataArray(dataPath = 'data/') {
+    //generated model data by vPAV
+    let dataFiles = ["checkers.js",
+        "bpmn_model.js",
+        "bpmn_validation.js",
+        "bpmn_validation_success.js",
+        "infoPOM.js",
+        "issue_severity.js",
+        "ignoredIssues.js",
+        "processVariables.js",
+        "properties.js"]
+    return dataFiles.map(file => dataPath + file);
+}
+
+function loadJs(js) {
+    js.forEach(scriptSrc => {
         let script = document.createElement("script");
         script.src = scriptSrc;
         script.async = false; //script tags added by DOM API are async by default (╯°□°）╯︵ ┻━┻
         document.body.appendChild(script);
     });
+}
+
+let dataFiles = generateJsDataArray();
+dataFiles.push('externalReports/reportPaths.js');
+loadJs(dataFiles);
+dataFiles = undefined;
+loadJs([
+    //bootstrap with dependencies
+    "js/jquery-3.5.1.min.js", "js/bootstrap.bundle.min.js",
+    //bpmn-js viewer
+    "js/bpmn-navigated-viewer.js",
+    //application
+    "js/download.js", "js/bpmn.io.viewer.app.js"]);
